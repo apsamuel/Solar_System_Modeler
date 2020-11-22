0ddc6649a947e747c997865791de3f71d7595ea7
apsamuel <aaron.psamuel@gmail.com>
2020-11-22 13:39:49 -0500

    modularize utility functions

---
+++ lib/utilz.py
@@ -0,0 +1,10 @@
+# general functions and common patterns 
+import ctypes
+
+# flatten nested arrays
+flatten = lambda t: [item for sublist in t for item in sublist]
+
+
+select = lambda array, item: [i[item] for i in array if item in i.keys()][0]
+
+make_integer_safe = lambda i: i-(i - ctypes.c_uint(-1).value)
