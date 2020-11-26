2dd3514f310a96baf387bab787afd3f13dbd9160
apsamuel <aaron.psamuel@gmail.com>
2020-11-26 15:48:29 -0500

    add utility function to ensure that integer values are below the C limit for int types

--- lib/utilz.py
+++ lib/utilz.py
@@ -1,10 +1,11 @@
 # general functions and common patterns 
 import ctypes
-
+from numpy import arange
 # flatten nested arrays
 flatten = lambda t: [item for sublist in t for item in sublist]
 
 
 select = lambda array, item: [i[item] for i in array if item in i.keys()][0]
 
-make_integer_safe = lambda i: i-(i - ctypes.c_uint(-1).value)
+make_integer_safe = lambda i: i - (i-ctypes.c_uint(-1).value)
+
