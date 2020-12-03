d1fdc7b0cb3d61868209bdeaa7645f9b377a32c5
apsamuel <aaron.psamuel@gmail.com>
2020-12-03 14:26:01 -0500

    add function for properly merging default scale setting dict with a user provided dict (user provided overrides defaults)

--- lib/utilz.py
+++ lib/utilz.py
@@ -1,6 +1,7 @@
 # general functions and common patterns 
 import ctypes
 from numpy import arange
+from copy import copy, deepcopy
 # flatten nested arrays
 flatten = lambda t: [item for sublist in t for item in sublist]
 
@@ -8,4 +9,17 @@ flatten = lambda t: [item for sublist in t for item in sublist]
 select = lambda array, item: [i[item] for i in array if item in i.keys()][0]
 
 make_integer_safe = lambda i: i - (i-ctypes.c_uint(-1).value)
+
+
+def merge_attributes(x ,y):
+    xcopy = deepcopy(x)
+    ycopy = deepcopy(y)
+    for nkey, nvalue in ycopy.items():
+        if nkey in xcopy: 
+            if isinstance(nvalue, dict):
+                for key, val in nvalue.items():
+                    xcopy[nkey][key] = val
+            else:
+                xcopy[nkey] = nvalue
+    return xcopy
 
