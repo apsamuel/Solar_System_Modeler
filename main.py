06317886750f0795417ca9c0c6c8b94793377b20
apsamuel <aaron.psamuel@gmail.com>
2020-12-28 07:11:53 -0500

    update main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import os, sys, importlib
 
 from copy import copy, deepcopy
-
+#LIB_HOME=os.path.dirname( os.path.realpath('__file__') )
 LIB_HOME='/Users/photon/DevOps/Projects/Solar_System_Model'
 os.chdir(LIB_HOME)
 sys.path.extend([os.path.join('.', 'lib')])
@@ -16,6 +16,7 @@ from planet import Planet
 from moon import Moon
 from sun import Sun
 from solarsystem import SolarSystem
+from texture import Tex
 import orbital as orbital
 import utilz as utilz
 import blender as blender 
@@ -23,6 +24,14 @@ import dev as dev
 
 # use properties of solar system to configure scene props, like a cool kid
 blender.scene_props(seperate_u=False)
+
+# instantiate and 'warmup' a texture library, use library features to autogenerate height, normal and specular maps
+
+# NOTE: 
+tex = Tex()
+tex.fetchitems(path=tex.default_path)
+tex.fetchitems(itemtypes=['height', 'normal', 'specular'],path=tex.default_path)
+
 
 # create the solar system (planets & moons), this takes some time 
 ss = SolarSystem(name='mysystem', debug=True)
@@ -54,7 +63,68 @@ blender.plot_planet(Planet.byname('Jupiter'), debug=True)
 blender.add_orbital_drivers(Planet.byname('Jupiter'))
 blender.plot_natural_satellites(Planet.byname('Jupiter'))
 
+blender.plot_planet(Planet.byname('Saturn'), debug=True)
+blender.add_orbital_drivers(Planet.byname('Saturn'))
+blender.plot_natural_satellites(Planet.byname('Saturn'))
+
+blender.plot_planet(Planet.byname('Uranus'), debug=True)
+blender.add_orbital_drivers(Planet.byname('Uranus'))
+blender.plot_natural_satellites(Planet.byname('Uranus'))
+
+blender.plot_planet(Planet.byname('Neptune'), debug=True)
+blender.add_orbital_drivers(Planet.byname('Neptune'))
+blender.plot_natural_satellites(Planet.byname('Neptune'))
+
+
 
 #example of texturing a planet
-blender.add_surface_displacement(Planet.byname('Earth'), '/Users/photon/Downloads/8k_earth_daymap_bump.jpg')
-blender.add_albedo(Planet.byname('Earth'), '/Users/photon/Downloads/8k_earth_daymap.jpg')
+
+blender.add_surface_displacement(ss.sun, tex.getattr('Sun','height'))
+#blender.add_texture(ss.sun, tex)
+blender.sun_texture(ss.sun, tex)
+blender.add_solar_dynamics(ss.sun)
+
+blender.add_surface_displacement(Planet.byname('Mercury'), tex.getattr('Mercury','height'))
+blender.add_texture(Planet.byname('Mercury'), tex)
+
+blender.add_surface_displacement(Planet.byname('Venus'), tex.getattr('Venus','height'))
+blender.add_texture(Planet.byname('Venus'), tex)
+
+blender.add_surface_displacement(Planet.byname('Earth'), tex.getattr('Earth','height'))
+blender.add_texture(Planet.byname('Earth'), tex)
+blender.add_surface_displacement(Moon.byname('Moon'), tex.getattr('Moon','height'))
+blender.add_texture(Moon.byname('Moon'), tex)
+
+blender.add_surface_displacement(Planet.byname('Mars'), tex.getattr('Mars','height'))
+blender.add_texture(Planet.byname('Mars'), tex)
+
+blender.add_surface_displacement(Planet.byname('Jupiter'), tex.getattr('Jupiter','height'))
+blender.add_texture(Planet.byname('Jupiter'), tex)
+
+blender.add_surface_displacement(Planet.byname('Saturn'), tex.getattr('Saturn','height'))
+blender.add_texture(Planet.byname('Saturn'), tex)
+
+blender.add_surface_displacement(Planet.byname('Uranus'), tex.getattr('Uranus','height'))
+blender.add_texture(Planet.byname('Uranus'), tex)
+
+
+blender.add_surface_displacement(Planet.byname('Neptune'), tex.getattr('Neptune','height'))
+blender.add_texture(Planet.byname('Neptune'), tex)
+
+
+# add tracking camera for planets 
+blender.add_planet_trackcam(Planet.byname('Mercury'))
+blender.add_planet_trackcam(Planet.byname('Venus'))
+blender.add_planet_trackcam(Planet.byname('Earth'))
+blender.add_planet_trackcam(Planet.byname('Mars'))
+blender.add_planet_trackcam(Planet.byname('Jupiter'))
+
+
+blender.plot_atmosphere(Planet.byname('Earth'))
+
+#blender.add_surface_displacement(Planet.byname('Jupiter'), tex.getattr('Jupiter','height'))
+#blender.add_surface_displacement(Planet.byname('Saturn'), tex.getattr('Saturn','height'))
+#blender.add_surface_displacement(Planet.byname('Uranus'), tex.getattr('Uranus','height'))
+#blender.add_surface_displacement(Planet.byname('Neptune'), tex.getattr('Neptune','height'))
+
+
